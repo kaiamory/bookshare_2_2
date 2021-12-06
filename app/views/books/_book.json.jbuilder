003c71c6eb316a2809c63716d179af28564550ab
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-12-06 02:17:18 +0000

    Generate books resource with ideas:scaffold

---
+++ app/views/books/_book.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! book, :id, :name, :author, :genre, :page_length, :user_id, :author_id, :created_at, :updated_at
+json.url book_url(book, format: :json)
---
+++ db/migrate/20211206021718_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.string :author
+      t.string :genre
+      t.string :page_length
+      t.integer :user_id
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+end
