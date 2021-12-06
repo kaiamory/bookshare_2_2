478850a3ee4ec4a67e543c57d46b64f636c67afa
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-12-06 02:18:21 +0000

    Generate graphiti resources for recommendations

---
+++ app/resources/recommendation_resource.rb
@@ -0,0 +1,13 @@
+class RecommendationResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :body, :string
+  attribute :sender_id, :integer
+  attribute :recipient_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
