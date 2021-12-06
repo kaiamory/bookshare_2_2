1e4e8f51f45c67dc674034106e0661911d68081f
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-12-06 02:18:50 +0000

    Integrate Ransack

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,8 @@ class BooksController < ApplicationController
 
   # GET /books
   def index
-    @books = Book.page(params[:page]).per(10)
+    @q = Book.ransack(params[:q])
+    @books = @q.result(:distinct => true).includes(:author_writers, :reviews, :users).page(params[:page]).per(10)
   end
 
   # GET /books/1
