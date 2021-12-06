b83896943d616282e4f069a90cbf6ee80f1e25ea
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-12-06 02:17:50 +0000

    Display associated collections on show pages

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -42,8 +42,14 @@ class AuthorsController < ApplicationController
   # DELETE /authors/1
   def destroy
     @author.destroy
-    redirect_to authors_url, notice: 'Author was successfully destroyed.'
+    message = "Author was successfully deleted."
+    if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+      redirect_back fallback_location: request.referrer, notice: message
+    else
+      redirect_to authors_url, notice: message
+    end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
