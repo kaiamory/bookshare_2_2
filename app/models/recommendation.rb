dae280dc0fae8e88dd1bee1e46fcd47a733edaa8
anonymous anonymous <kai.amory@kellogg.northwestern.edu>
2021-12-06 02:17:26 +0000

    Direct association between users and recommendations

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,6 +1,10 @@
 class Recommendation < ApplicationRecord
   # Direct associations
 
+  belongs_to :recipient,
+             :class_name => "User",
+             :foreign_key => "sender_id"
+
   belongs_to :sender,
              :class_name => "User",
              :foreign_key => "body"
